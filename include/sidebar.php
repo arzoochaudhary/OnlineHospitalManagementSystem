0cfb0572cf9f67ae8a4e56765ccd2dd70136fe1b
arzoochaudhary <86251456+arzoochaudhary@users.noreply.github.com>
2021-08-23 12:21:17 +0530

    Add files via upload

---
+++ include/sidebar.php
@@ -0,0 +1,58 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+									
+										<div class="item-inner">
+											<span class="title"> Dashboard </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							
+
+							<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<!---<div class="item-media">
+											<i class="ti-list"></i>
+										</div>--->
+										<div class="item-inner">
+											<span class="title"> Appointment History </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										
+										<div class="item-inner">
+										<a href="add-patient.php">
+											<span class="title"> Add Patients </span>
+										</a>
+									
+										</div>
+									</div>
+								</a>
+								
+							</li>
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
+
+
+
